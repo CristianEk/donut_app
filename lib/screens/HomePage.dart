51bf844e288245e4775d6f2d394afe86baa2c7fd
CristianEk <crisekperera05@gmail.com>
2025-10-09 16:31:53 -0600

    AddBar_added

---
+++ lib/screens/HomePage.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _MyWidgetState();
+}
+
+class _MyWidgetState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        ///icono
+        leading: Icon(Icons.menu, color:Colors.grey[800]),
+        //icono de la derecha
+        actions:[ 
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person),
+          )
+        ]
+      ),
+      //
+      body: const Column(
+        children: [
+          //texto principal
+          //pestañas/ tab bar
+          //contenido
+          //carrito
+        ],
+      )
+    );
+  }
+}
